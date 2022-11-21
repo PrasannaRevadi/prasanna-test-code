968c94a11e062564f8e70ff13f311373a379499b
RevadiPrasanna <118728036+PrasannaRevadi@users.noreply.github.com>
2022-11-21 16:12:22 +0530

    typeface

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
-def numberToBase(n, b):
+def numberToBase(n, 3):
     if n == 0:
         return [0]
     digits = []
     while n:
-        digits.append(int(n % b))
-        n //= b
+        digits.append(int(n % 3))
+        n //= 3
     return digits[::-1]
